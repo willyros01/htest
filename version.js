8f71360985719c3a9b7812f9f5796ee483b0e9af
willyros01 <willyros01@gmail.com>
2026-09-06 03:01:32 -0400

    Add pinned FIDUNIO test version

---
+++ version.js
@@ -0,0 +1,2 @@
+/* FIDUNIO 0.9.6.4 isolated GitHub Pages test host. Source pinned to Hermes rebuild commit bda5efee19333b4b3a3108341d30431d20447754. */
+globalThis.FIDUNIO_RELEASE = Object.freeze({ version: "0.9.6.4" });
