95eff0bfdf0fa272923948c4c437ef55be1bd20d
willyros01 <willyros01@gmail.com>
2026-09-06 04:03:36 -0400

    Add verified FIDUNIO public Firebase web configuration

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDHrsQouUa1Ud-a0yivEndrPGoSR9AEGLs",
+  authDomain: "fidunio-fef13.firebaseapp.com",
+  projectId: "fidunio-fef13",
+  storageBucket: "fidunio-fef13.firebasestorage.app",
+  messagingSenderId: "130339622893",
+  appId: "1:130339622893:web:912a9016e294b30dd0e1e4"
+};
