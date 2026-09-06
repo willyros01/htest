55f1f36f1ed090665bbb5caa2ec698e7bc7aeab0
willyros01 <willyros01@gmail.com>
2026-09-06 04:03:20 -0400

    Use authoritative FIDUNIO version file

--- version.js
+++ version.js
@@ -1,2 +1,4 @@
-/* FIDUNIO 0.9.6.4 isolated GitHub Pages test host. Source pinned to Hermes rebuild commit bda5efee19333b4b3a3108341d30431d20447754. */
-globalThis.FIDUNIO_RELEASE = Object.freeze({ version: "0.9.6.4" });
+/* FIDUNIO single authoritative release version. */
+globalThis.FIDUNIO_RELEASE = Object.freeze({
+  version: "0.9.6.4"
+});
